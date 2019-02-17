3b5518853c3a6e40acecf353c8cc3fcb8f003b70
pkapu001 <pkapu001@odu.edu>
2019-02-17 13:58:33 +0700

    UI modified

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,11 +4,19 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const background = document.querySelector(".blur");
+const job = document.querySelectorAll(".job");
+const about = document.querySelector(".about");
 
 // initial state
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+//job.addEventListener("mouseover", itsin);
+//job.addEventListener("mouseout", itsout);
+
+job.forEach(item => item.addEventListener("mouseover", itsin));
+job.forEach(item => item.addEventListener("mouseout", itsout));
 
 function toggleMenu() {
   if (!showMenu) {
@@ -17,6 +25,7 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
+    background.classList.add("blurnow");
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
@@ -24,6 +33,14 @@ function toggleMenu() {
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
+    background.classList.remove("blurnow");
     showMenu = false;
   }
+}
+
+function itsin() {
+  background.classList.add("darken");
+}
+function itsout() {
+  background.classList.remove("darken");
 }
